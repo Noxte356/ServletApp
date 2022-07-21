de6edbf713396927866849befe43d40ea70a7579
Egor Khomyakov <egor-khomyakov@mail.ru>
2022-07-22 02:07:16 +0300

    good

--- src/main/java/packageEK/RequestExtractor.java
+++ src/main/java/packageEK/RequestExtractor.java
@@ -3,11 +3,15 @@ package packageEK;
 public class RequestExtractor {
     public User parsing(String request){
         User user = new User();
-        String[] userData = request.split("&");
-        user.setId(userData[0]);
-        user.setName(userData[1]);
-        user.setMail(userData[2]);
-        user.setPassword(userData[3]);
+        String[] userData = request.split("=");
+        String[] id = userData[0].split("&");
+        user.setId(id[0]);
+        String[] name = userData[1].split("&");
+        user.setName(name[0]);
+        String[] mail = userData[2].split("&");
+        user.setMail(mail[0]);
+        String[] password = userData[3].split("&");
+        user.setPassword(password[0]);
         return user;
     }
 }
--- src/main/java/packageEK/UserSaver.java
+++ src/main/java/packageEK/UserSaver.java
@@ -6,10 +6,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserSaver {
-    public void save(User user) {
+//    private User user;
+
+    public UserSaver() {
+    }
+
+    public void save(User client) {
+        User user = client;
         String urlJdbc = "jdbc:postgresql://localhost:5432/usersDataBase";
         String sqlUserName = "postgres";
         String sqlPassword = "";
+        String line = "VALUES (%s,'%s','%s','%s')";
+        String sql = String.format(line,user.getName(),user.getMail(),user.getPassword());
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
@@ -19,8 +27,10 @@ public class UserSaver {
         try {
             Connection connection = DriverManager.getConnection(urlJdbc,sqlUserName,sqlPassword);
             System.out.println("Connection accept");
-            String insert = "INSERT INTO public.users (user_id, user_name, user_mail, user_password)" +
-                    "VALUES (12114,homyak,homa@mail.com,password)";
+            String insert = "INSERT INTO public.users (user_id, user_name, user_mail, user_password)" + sql;
+//                    "VALUES " + "(" + user.getId() + "," + "'" + user.getName() + "'" + "," +
+//                    "'" + user.getMail() + "'" + "'" + user.getPassword() + "'" + ")";
+//                    "VALUES (1214,'homka','homa@mail.com','password')";
             Statement statement = connection.createStatement();
             int a = statement.executeUpdate(insert);
             if (a>0){
